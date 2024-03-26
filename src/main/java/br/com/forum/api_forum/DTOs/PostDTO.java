9c58bbe1b3892a40868ae4defbb7f752330cfaeb
caio-santos-ios <caiosantos.dev@outlook.com>
2024-03-25 22:09:25 -0300

    add branch develop

---
+++ src/main/java/br/com/forum/api_forum/DTOs/PostDTO.java
@@ -0,0 +1,5 @@
+package br.com.forum.api_forum.DTOs;
+
+public class PostDTO {
+    
+}
---
+++ src/main/java/br/com/forum/api_forum/Entities/PostEntity.java
@@ -0,0 +1,26 @@
+package br.com.forum.api_forum.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "posts")
+public class PostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private int likes;
+
+    @Column
+    private int deslikes;
+}
